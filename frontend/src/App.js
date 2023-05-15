e8a8f2575bbe5c77566d2e1c2dd4566643ea8337
OgBludo <mihailkadyrov2002@mail.ru>
2023-05-15 18:55:18 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
 
 import { Button } from '@chakra-ui/react';
 import './App.css';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route,Routes } from 'react-router-dom/cjs/react-router-dom.min';
 import HomePage from './Pages/HomePage';
 import ChatPage from './Pages/ChatPage';
 
 function App() {
   return (
-    <div className="App">
+    < >
       <Route path='/' component={HomePage} exact>
 
       </Route>
       <Route path='/chats'component={ChatPage}>
         
       </Route>
-    </div>
+    </>
   );
 }
 
